d70b94127f1e780fe902fbdb7a424de208750a0a
Poopi0990 <kaslox@o2.pl>
2025-06-04 22:58:55 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,1 +1,10 @@
+-- 1. Średni czas dostawy (ShippedDate - OrderDate) dla krajów
+-- Celem jest sprawdzenie, ile średnio dni trwa dostawa zamówienia
+-- (Można użyć funkcji julianday do obliczenia różnicy dat)
 
+SELECT O.ShipCity || ',' || o.ShipCountry AS ShipLocation, C.Country AS CustomerCountry,
+  ROUND(AVG(julianday(ShippedDate) - julianday(OrderDate)), 2) AS avg_delivery_days
+FROM Orders as O
+LEFT JOIN CUSTOMERS AS C ON O.CustomerID = C.CustomerID
+GROUP BY ShipCity, C.Country
+ORDER BY avg_delivery_days DESC;
